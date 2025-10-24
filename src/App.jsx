018f94f53c0a7d4f783153a8ec53c1a75343469a
dianakius <eingidiana@gmail.com>
2025-10-24 17:36:48 +0300

    feat: Import Header,AboutPage components and add them into the return

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -33,24 +35,28 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            filter={filter}
-            setFilter={setFilter}
-            limit={limit}
-            setLimit={setLimit}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            loading={loading}
-            error={error}
-          />
-        }
-      />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 };
 
