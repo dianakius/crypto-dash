2c9c554742f3393f278f5f3b35be40aed9db14e6
dianakius <eingidiana@gmail.com>
2025-10-24 17:36:07 +0300

    feat: Create an AboutPage.jsx component

---
+++ src/pages/about.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1> About Crypto Dash</h1>
+      <p>
+        {" "}
+        Crypto Dash is a simple and intuitive cryptocurrency dashboard
+        application that allows users to track and analyze various
+        cryptocurrencies in real-time. The application provides essential
+        features such as filtering, sorting, and limiting the number of
+        displayed cryptocurrencies to enhance user experience.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
