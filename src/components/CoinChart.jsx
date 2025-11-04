eb506da5bdf9ee0d7ca5e0894021447d67d9e2c6
dianakius <eingidiana@gmail.com>
2025-11-04 14:57:33 +0200

    feat: Create the chart

--- src/components/CoinChart.jsx
+++ src/components/CoinChart.jsx
@@ -60,7 +60,39 @@ const CoinChart = ({ coinId }) => {
     fetchPrices();
   }, [coinId]);
 
-  return <>Chart</>;
+  if(loading) return <p>Loading chart...</p>;
+
+  return (
+    <div style={{ marginTop: '30px' }}>
+      <Line
+        data={chartData}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: { display: false },
+            tooltip: { mode: 'index', intersect: false },
+          },
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day',
+              },
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 7,
+              },
+            },
+            y: {
+              ticks: {
+                callback: (value) => `$${value.toLocaleString()}`,
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default CoinChart;
